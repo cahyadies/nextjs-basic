f1d81d5629df4dcc6062b9d3d8cb0b294a503dab
Eko Sugeng Cahyadi <ekosugengcahyadi@gmail.com>
2021-11-07 07:12:58 +0700

    Creat NextJS Project

---
+++ pages/users/detail.tsx
@@ -0,0 +1,7 @@
+export default function DetailUsers() {
+    return (
+        <div>
+            <p>Detail User</p>            
+        </div>
+    )
+}
---
+++ pages/users/index.tsx
@@ -0,0 +1,9 @@
+
+
+export default function IndexUsers() {
+    return (
+        <div>
+            <p>Halaman Index Users</p>
+        </div>
+    )
+}
